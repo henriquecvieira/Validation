225c433f9f410e08be294526fd54c8b9fee16179
Henrique Vieira <87788394+henriquecvieira@users.noreply.github.com>
2022-05-19 12:09:18 -0300

    validando formulários

--- index.js
+++ index.js
@@ -17,11 +17,42 @@ app.use(session({
     cookie: { secure: true }
   }))
 
-  app.use(flash())
+app.use(flash())
 
-  app.get('/', (req, res) => {
+app.get('/', (req, res) => {
      res.render("index")
     })
+
+app.post("/form", (req, res) => {
+    const {email, nome, pontos} = req.body
+     let emailError
+     let nomeError
+     let pontosError
+
+    if(email == undefined || email == ""){
+        emailError = "O email não pode ser vazio" 
+    }
+    if(nome == undefined || nome == "" ){
+        nomeError = "O nome não pode ser vazio"
+        
+    }
+    if(pontos == undefined || pontos  < 20){
+        pontosError = "Você não pode ter menos de 20 pontos"
+    }
+    if(nome.length < 4){
+        nomeError = "O nome é muito pequeno"
+        
+    }
+    
+
+    if(emailError != undefined || nomeError != undefined || pontosError != undefined){
+        res.redirect("/")
+    }else{
+        res.send("succes, show de bola")
+    }
+
+    
+})
 
 
 app.listen("3333", (req, res) =>{
